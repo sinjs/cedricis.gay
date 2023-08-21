82082b498209c9191c95a06d42d67cede5be87af
sin <50749166+sinjs@users.noreply.github.com>
2023-08-21 22:45:48 +0200

    add pernes client teaser

---
+++ pages/pernes.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-zinc-900 rounded-xl p-4 my-3">
+    <h1 class="text-center text-5xl font-extrabold">Pernes Client ⛏️</h1>
+    <h2 class="text-center text-2xl font-semibold mt-2">
+      Coming soon to a store near you...
+    </h2>
+  </div>
+</template>
